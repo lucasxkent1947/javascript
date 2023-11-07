597b8eeaf824aef5ebb6506db136b99e86739142
Lucas Rodriguez <xlucasrodriguez@gmail.com>
2023-11-07 20:36:55 -0300

    Clase 7/Noviembre

---
+++ otro.js
@@ -0,0 +1,61 @@
+let nombreIngresado = prompt("Ingresá tu nombre: ")  //PROMPT PARA PEDIR DATOS DE ENTRADA = QUE EL USUARIO INGRESE ALGO
+
+console.log("Lo que escriba acá, va a aparecer en la consola") // Nos muestra en Consola lo que le pasemos entre los parentesis.
+
+
+//COMENTARIO UNA LINEA LO QUE QUERRAMOS ESCRIBIR
+
+
+/* ESTO ES 
+UN 
+COMENTARIO
+MULTILINEA */
+
+/* PALABRAS RESERVADAS: 
+break, case, catch, continue, default, let, delete, do, else, finally, for, ir, in , return, try, var, function, while y un monton más */
+
+//Declaracion = CREAR UNA VARIABLE
+// let apellido
+
+//ASIGNAR
+
+/* apellido = "Rodriguez";
+miClub = "Morón"; */
+
+
+
+//INICIALIZAR
+let apellido = "Rodriguez";
+
+const miBarrio = "Viejo Urbano";
+
+
+let numeroA = 1;
+let numeroB = 2;
+const NUMEROC = 3;
+//Suma de dos numeros
+let resultadoSuma = numeroA + numeroB;
+alert(resultadoSuma)
+// Resta de dos numeros
+let resultadoResta = numeroB - numeroA;
+alert(resultadoResta)
+//Multiplicacion 
+let resultadoProducto = numeroB * NUMEROC;
+
+
+//CONCATENAR STRINGS
+
+let textoA = "Front";
+let textoB = "End";
+const BLANCO = " ";
+
+//CONCATENAR AMBOS VALORES
+
+let resultadoA = textoA + textoB;  //FrontEnd
+alert(resultadoA) //En el ALERT escribimos entre parentesis la variable a mostrar - En este caso nos devuelve el resultado
+let resultadoB = textoA + 1;
+alert(resultadoB)
+let resultadoC = textoA + BLANCO + textoB;
+console.log(resultadoC)
+
+
